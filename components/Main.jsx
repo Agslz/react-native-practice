41c508ff22172ae66289195046b34df18f42e45e
Agslz <agsp3rs3o@gmail.com>
2024-10-04 21:09:21 -0300

    Part 1 Finished

---
+++ components/Main.jsx
@@ -0,0 +1,42 @@
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  ScrollView,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { getLatestGames } from "../lib/metacritic";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { GameCard } from "./GameCard";
+import { ActivityIndicator } from "react-native";
+import { Logo } from "./Logo";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+        <View style={{ marginBottom: 20}}>
+        <Logo/>
+        </View>
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item }) => <GameCard game={item} />}
+        />
+      )}
+    </View>
+  );
+}
