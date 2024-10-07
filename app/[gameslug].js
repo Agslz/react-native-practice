09b3d945a94ebd59268a79b942904df728de42c5
Agslz <agsp3rs3o@gmail.com>
2024-10-07 17:38:42 -0300

    Fix Game Indicator header and typo in API

--- app/[gameslug].js
+++ app/[gameslug].js
@@ -1,20 +1,43 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
 import { Screen } from "../components/Screen";
+import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { getGameDetails } from "../lib/metacritic";
 
 export default function Detail() {
   const { gameslug } = useLocalSearchParams();
+  const [gameInfo, setGameInfo] = useState(null);
+
+  useEffect(() => {
+    if (gameslug) {
+      getGameDetails(gameslug).then(setGameInfo);
+    }
+  });
 
   return (
     <Screen>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#ffee00" },
+          headerTintColor: "black",
+          headerLeft: () => {},
+          headerTitle: "The Legend of Zelda",
+          headerRight: () => {},
+        }}
+      />
+
       <View>
-        <Text className="text-white font-bold mb-8 text-2xl">
-          Detalle del juego {gameslug}
-        </Text>
-        <Link href="/" className="text-blue-500">
-          Volver atrás
-        </Link>
+        {gameInfo === null ? (
+          <ActivityIndicator color={"#fff"} size={"large"} />
+        ) : (
+          <ScrollView>
+            <Text className="text-white font-bold mb-8 text-2xl">
+              Detalle del juego {gameslug}
+            </Text>
+          </ScrollView>
+        )}
       </View>
     </Screen>
   );
