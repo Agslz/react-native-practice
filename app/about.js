48543c52f020da62bcbde25880db5e6e815a333d
Agustín Schulze <agsp3rs3o@gmail.com>
2024-10-07 12:07:15 -0300

    Adding Routes and Reforming Icons

---
+++ app/about.js
@@ -0,0 +1,85 @@
+import { Pressable, ScrollView, Text } from "react-native";
+import { Link } from "expo-router";
+import { HomeIcon } from "../components/Icons";
+import { styled } from "nativewind";
+
+export default function About() {
+  const StyledPressable = styled(Pressable);
+
+  return (
+    <ScrollView className="pt-24">
+      <Link asChild href="/">
+        <StyledPressable className={`active:opacity-50`}>
+          <HomeIcon />
+        </StyledPressable>
+      </Link>
+
+      <Text className="text-white font-bold mb-8 text-2xl">
+        Sobre el Proyecto
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+      <Text className="text-white text-white/90 mb-4">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </Text>
+    </ScrollView>
+  );
+}
--- components/Main.jsx
+++ components/Main.jsx
@@ -4,18 +4,23 @@ import {
   Text,
   Image,
   View,
+  Pressable,
   ScrollView,
 } from "react-native";
+import { Link } from "expo-router";
 import { useEffect, useState } from "react";
 import { getLatestGames } from "../lib/metacritic";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { GameCard } from "./GameCard";
 import { ActivityIndicator } from "react-native";
 import { Logo } from "./Logo";
+import { CircleInfoIcon } from "../components/Icons";
+import { styled } from "nativewind";
 
 export function Main() {
   const [games, setGames] = useState([]);
   const insets = useSafeAreaInsets();
+  const StyledPressable = styled(Pressable);
 
   useEffect(() => {
     getLatestGames().then((games) => {
@@ -25,9 +30,16 @@ export function Main() {
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
-        <View style={{ marginBottom: 20}}>
-        <Logo/>
-        </View>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
+
+      <Link asChild href="/about">
+        <StyledPressable className={`active:opacity-50`}>
+          <CircleInfoIcon />
+        </StyledPressable>
+      </Link>
+
       {games.length === 0 ? (
         <ActivityIndicator color={"#fff"} size={"large"} />
       ) : (
